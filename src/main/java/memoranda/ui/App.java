bd9eef483985cc1ca77f6fd21f7411ac2ba2300d
spaceperson <mawebbe1@asu.edu>
2018-02-05 13:04:39 -0700

    Changed Splash Screen and added delay by 1.5 seconds
    in main.java.memoranda.ui -> App.java
    
    changed the splash screen image in showSplash()
    and updated the new image dimensions.

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -13,6 +13,7 @@ import javax.swing.UIManager;
 import main.java.memoranda.EventsScheduler;
 import main.java.memoranda.util.Configuration;
 
+
 /**
  * 
  * Copyright (c) 2003 Memoranda Team. http://memoranda.sf.net
@@ -130,12 +131,28 @@ public class App {
 		}
 		/* --------------------------------------------------------------- */
 		/* Added By Jeremy Whitlock (jcscoobyrs) 07-Nov-2003 at 15:54:24 */
+		
+		
+		// Waits for 1.5 seconds before closing the splash screen
+		// ------------------------------------- mawebbe1
+		// Sources: https://stackoverflow.com/questions/24104313/how-to-delay-in-java
+		// Ann Ragg
+		try        
+		{
+		    Thread.sleep(1500);
+		} 
+		catch(InterruptedException ex) 
+		{
+		    Thread.currentThread().interrupt();
+		}
+		// ------------------------------------- mawebbe1
+		
 
 		// Not needed ???
 		frame.setVisible(true);
 		frame.toFront();
 		frame.requestFocus();
-
+		
 	}
 
 	public static void closeWindow() {
@@ -150,16 +167,18 @@ public class App {
 	private void showSplash() {
 		splash = new JFrame();
 		ImageIcon spl =
-			new ImageIcon(App.class.getResource("/ui/splash.png"));
+			new ImageIcon(App.class.getResource("/ui/splash3.png"));
 		JLabel l = new JLabel();
-		l.setSize(400, 300);
+		//splash  dimensions 400, 300
+		//splash2 dimensions 672, 378
+		l.setSize(672, 378);
 		l.setIcon(spl);
 		splash.getContentPane().add(l);
-		splash.setSize(400, 300);
+		splash.setSize(672, 378);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		splash.setLocation(
-			(screenSize.width - 400) / 2,
-			(screenSize.height - 300) / 2);
+			(screenSize.width - 672) / 2,
+			(screenSize.height - 378) / 2);
 		splash.setUndecorated(true);
 		splash.setVisible(true);
 	}
