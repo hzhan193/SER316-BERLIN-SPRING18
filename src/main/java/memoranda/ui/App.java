efcc2dcf19ce41c6e99618f750639cdba6ebdd8d
pkburke <pkburke@asu.edu>
2018-02-07 12:05:15 -0700

    Revert "Merge pull request #3 from ser316asu/SplashScreen"
    This reverts commit b4e77ac2420e3223b12465026ca5e6a8ab512101, reversing
    changes made to 913d1f2608c2ccbbe97002cbf3de1f969e5af640.

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -15,7 +15,6 @@ import javax.swing.UIManager;
 import main.java.memoranda.EventsScheduler;
 import main.java.memoranda.util.Configuration;
 
-
 /**
  * 
  * Copyright (c) 2003 Memoranda Team. http://memoranda.sf.net
@@ -133,22 +132,6 @@ public class App {
 		}
 		/* --------------------------------------------------------------- */
 		/* Added By Jeremy Whitlock (jcscoobyrs) 07-Nov-2003 at 15:54:24 */
-		
-		
-		// Waits for 1.5 seconds before closing the splash screen
-		// ------------------------------------- mawebbe1
-		// Sources: https://stackoverflow.com/questions/24104313/how-to-delay-in-java
-		// Ann Ragg
-		try        
-		{
-		    Thread.sleep(1500);
-		} 
-		catch(InterruptedException ex) 
-		{
-		    Thread.currentThread().interrupt();
-		}
-		// ------------------------------------- mawebbe1
-		
 
 		// Not needed ???
 		frame.setVisible(true);
@@ -170,18 +153,16 @@ public class App {
 	private void showSplash() {
 		splash = new JFrame();
 		ImageIcon spl =
-			new ImageIcon(App.class.getResource("/ui/splash3.png"));
+			new ImageIcon(App.class.getResource("/ui/splash.png"));
 		JLabel l = new JLabel();
-		//splash  dimensions 400, 300
-		//splash2 dimensions 672, 378
-		l.setSize(672, 378);
+		l.setSize(400, 300);
 		l.setIcon(spl);
 		splash.getContentPane().add(l);
-		splash.setSize(672, 378);
+		splash.setSize(400, 300);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		splash.setLocation(
-			(screenSize.width - 672) / 2,
-			(screenSize.height - 378) / 2);
+			(screenSize.width - 400) / 2,
+			(screenSize.height - 300) / 2);
 		splash.setUndecorated(true);
 		splash.setVisible(true);
 	}
